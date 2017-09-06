b90eedcc60113c60c697e138cf304122e1c18b57
Pablo Varela <pablovarela182@gmail.com>
2017-09-06 16:27:05 +0200

    Check for empty or 404

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
+const {send} = require('micro')
 const fetch = require('isomorphic-fetch')
 const url = require('url')
 
 module.exports = async (req, res) => {
   const href = url.parse(req.url).href
+  if (href === '/') {
+    send(res, 400, 'Empty request')
+    return
+  }
   const requestedUrl = `http:/${href}`
-  const fetched = await fetch(requestedUrl)
-  const body = await fetched.text()
-  res.end(body)
+  fetch(requestedUrl)
+    .then(async fetched => {
+      const body = await fetched.text()
+      res.end(body)
+    })
+    .catch(() => {
+      send(res, 404, 'Not found')
+    })
 }
