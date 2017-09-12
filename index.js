43e8446fa468e6589bfef0b53d743344680fe8cd
Pablo Varela <pablovarela182@gmail.com>
2017-09-12 18:19:20 +0200

    Add header and refactor

--- index.js
+++ index.js
@@ -2,15 +2,34 @@ const {send} = require('micro')
 const fetch = require('isomorphic-fetch')
 const url = require('url')
 
+const setResponseHeaders = (headers, res) => {
+  for (let h in headers) {
+    res.setHeader(h, headers[h])
+  }
+  return res
+}
+
+const defaultResponseHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers': '*'
+}
+
+const removeHeaderFromRequest = (req, header) => {
+  delete req.headers[header]
+  return req
+}
+
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
+  res = setResponseHeaders(defaultResponseHeaders, res)
   const href = url.parse(req.url).href
   if (href === '/') {
     send(res, 400, 'Empty request')
     return
   }
   const requestedUrl = `http:/${href}`
-  delete req.headers.host
+
+  removeHeaderFromRequest(req, 'host')
+
   fetch(requestedUrl, {headers: req.headers})
     .then(async fetched => {
       const body = await fetched.text()
